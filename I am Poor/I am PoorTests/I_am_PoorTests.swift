f5639c869e8581947971e677c3ddf277367f0456
sedtyldz <ikinciyargic@gmail.com>
2025-01-12 02:19:56 +0300

    UIKIT ilk projeler

---
+++ I am Poor/I am PoorTests/I_am_PoorTests.swift
@@ -0,0 +1,17 @@
+//
+//  I_am_PoorTests.swift
+//  I am PoorTests
+//
+//  Created by Sedat Yıldız on 10.01.2025.
+//
+
+import Testing
+@testable import I_am_Poor
+
+struct I_am_PoorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
