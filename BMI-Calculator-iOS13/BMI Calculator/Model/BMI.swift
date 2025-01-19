49c0d1826faf032bd27e23a2530c5e3aabb0da1f
Sedat Yıldız <121622443+sedtyldz@users.noreply.github.com>
2025-01-19 22:58:12 +0300

    BMI Calculator APP  Learning MVC,Segue,Classes

---
+++ BMI-Calculator-iOS13/BMI Calculator/Model/BMI.swift
@@ -0,0 +1,16 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Sedat Yıldız on 19.01.2025.
+//  Copyright © 2025 Angela Yu. All rights reserved.
+//
+
+import Foundation
+import UIKit
+struct BMI {
+    
+    let value: Float
+    let advice: String
+    let color : UIColor
+}
