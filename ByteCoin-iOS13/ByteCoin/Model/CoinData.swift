1e0c8255c5c0d7484619dd849f3580821eff58c9
Sedat Yıldız <121622443+sedtyldz@users.noreply.github.com>
2025-03-23 09:49:28 +0300

    Add files via upload

---
+++ ByteCoin-iOS13/ByteCoin/Model/CoinData.swift
@@ -0,0 +1,14 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Sedat Yıldız on 30.01.2025.
+//  Copyright © 2025 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+
+struct CoinData: Decodable {
+ let rate:Double
+}
