4c07e0405581b9c3b67f58ba4710698f2d32433e
Sedat Yıldız <121622443+sedtyldz@users.noreply.github.com>
2025-03-23 10:00:52 +0300

    Add files via upload

---
+++ Flash-Chat-iOS13/Flash Chat iOS13/Message.swift
@@ -0,0 +1,15 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Sedat Yıldız on 20.03.2025.
+//  Copyright © 2025 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+
+struct Message {
+    let sender : String
+    let body : String
+}
